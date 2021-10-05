27333c7486d9694eb7aca83c5f344ef6523eb3e4
Diogo Oliveira <diogo.o@icloud.com>
2021-10-05 11:27:32 -0300

    add photo endpoint + add city name to property

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@
 
 use Illuminate\Support\Facades\DB;
 
+// $router->post('/token', function () use ($router){
+//     $token = date('y'.'C77656'.'y'.'CC802'.'mm'.'29EC6'.'dy'.'W27TEQ'.'yd'. '0870'.'my'.'E285'.'yd'.'471'); 
+//     return $token;
+// });
+
+
 /** Diogo Oliveira - 17-09-2021
  * executa o mapeamento de campos correspondentes do IF2 para o CRM
  * @param json | $db nome do database do cliente
@@ -28,23 +34,26 @@ $router->post('/properties', function () use ($router){
     //tentativa de conexão com o banco utilizando PDO e o db fornecido pela requisição
     try{
         $pdo = new PDO('mysql:host=78.47.208.5;dbname='.$db, 'diogo.oliveira', ':4&find&BOOK&6:');
+        // $pdo = new PDO('mysql:host=localhost;dbname='.$db, 'root', '');
     } catch (PDOException $Exception){
         return response()->json($Exception->getMessage());
     }
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
     //query de consulta ao banco
-    $consulta = $pdo->query("SELECT p.id, p.reference, p.status, p.situation, p.exclusive, p.position, p.zone,
-        p.constructed_year, p.reform_year, p.solar_orientation, p.schedule_visit, p.financing, p.exchange_accept, p.bedroom, p.suite, p.bathroom,
-        p.kitchen, p.vacancy, p.housemaidroom, p.room, p.hobby_box, p.currency, p.main_purpose, p.hide_price, p.valued_sale,
-        p.commission_broker, p.valued_rent, p.valued_season, p.exchange_property_value, p.iptu_price, p.iptu_period,
-        parcels, p.condo_price, p.usefull_area_measure, p.constructed_area_measure, p.private_area_measure,
-        p.common_area_measure, p.terrain_area_measure, p.total_area_measure, p.condo_id, p.zipcode,
-        p.country, p.estate, p.city_id, p.neighborhood_id, p.street, p.number, p.complement, p.reference,
+    $consulta = $pdo->query("select c.name as city_id, p.id, p.reference, p.status, p.situation, p.exclusive, p.position, p.zone,
+        p.constructed_year, p.reform_year, p.solar_orientation, p.schedule_visit, p.financing, 
+        p.exchange_accept, p.bedroom, p.suite, p.bathroom, p.kitchen, p.vacancy, p.housemaidroom, 
+        p.room, p.hobby_box, p.currency, p.main_purpose, p.hide_price, p.valued_sale,
+        p.commission_broker, p.valued_rent, p.valued_season, p.exchange_property_value, p.iptu_price, 
+        p.iptu_period, p.parcels, p.condo_price, p.usefull_area_measure, p.constructed_area_measure, 
+        p.private_area_measure, p.common_area_measure, p.terrain_area_measure, p.total_area_measure, p.condo_id, zipcode,
+        p.country, p.estate, p.neighborhood_id, p.street, p.number, p.complement, p.reference,
         p.block, p.lat, p.lng, p.description, p.obs, p.details, p.publish_title, p.web_title, p.seo_tag_title,
         p.seo_url, p.seo_meta_key_words, p.seo_meta_tag_description, p.main_video_url, p.created_at,
         p.updated_at, p.deleted_at
-        FROM property p");
+        FROM property p
+        left join city c on c.id = p.city_id;");
 
     
     // criação da lista vazia
@@ -289,6 +298,65 @@ $router->post('/condominiums', function () use ($router){
             'updated_at' => utf8_encode($linha['updated_at']),
             'deleted_at' => utf8_encode($linha['deleted_at']),
 
+        ]);
+    }
+
+return json_encode($result);
+});
+
+$router->post('/photo', function () use ($router){
+    //gerando token de acordo com a data de hoje
+    $token = date('y'.'C77656'.'y'.'CC802'.'mm'.'29EC6'.'dy'.'W27TEQ'.'yd'. '0870'.'my'.'E285'.'yd'.'471'); 
+    
+    // recebendo token da requisição e db do cliente
+    $token_request = $_POST["token"] ?? null;
+    $db = $_POST["db"] ?? null;
+
+    //validando token recebido
+    if( $token != $_POST["token"]) {
+        return response()->json('Token Inválido', 401); 
+    } 
+
+    // capturando o nome do cliente
+    $cliente = str_replace('_if2', '', $db);
+
+    //tentativa de conexão com o banco utilizando PDO e o db fornecido pela requisição
+    try{
+        $pdo = new PDO('mysql:host=78.47.208.5;dbname='.$db, 'diogo.oliveira', ':4&find&BOOK&6:');
+    } catch (PDOException $Exception){
+        return response()->json($Exception->getMessage());
+    }
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "
+        SELECT f.id, f.name 
+        FROM photo f 
+        inner join property p on p.id = f.property_id
+        where p.status != 2
+        order by p.status desc;
+    ";
+
+    //query de consulta ao banco
+    $consulta = $pdo->query("SELECT f.id, f.name, f.width, f.height, f.thumb_width, f.property_id, f._order 
+    FROM photo f;");
+
+
+
+    // criação da lista vazia
+    $result=[];
+
+    // percorrendo a lista com os resultados da consulta e retornando uma lista adaptada ao banco do CRM
+    while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+        array_push($result, [
+
+            'id' => utf8_encode($linha['id']),
+            'name' => utf8_encode($linha['name']),
+            'url' => 'https://static.if2.com.br/acc/'.$cliente.'/photos/'.$linha['property_id'].'/'.$linha['name'].'.jpg',
+            // 'width' => utf8_encode($linha['width']),
+            // 'height' => utf8_encode($linha['height']),
+            // 'thumb_width' => utf8_encode($linha['thumb_width']),
+            'property_id' => utf8_encode($linha['property_id']),
+            'order' => utf8_encode($linha['_order'])
         ]);
     }
 
